e6651c2e379c6287eeb8b30295c6e3d0c055deb8
dollminnyy-a11y <dollminnyy@gmail.com>
2026-03-23 11:33:15 -0300

    Initialize Shopee.Node.js project with description
    Add project description and GitHub link for Shopee.Node.js

---
+++ Shopee.Node.js
@@ -0,0 +1,3 @@
+https://github.com/dollminnyy/shopee-cart-node
+Este projeto consiste na implementação de um carrinho de compras inspirado na plataforma Shopee, desenvolvido utilizando Node.js. A aplicação simula o funcionamento básico de um sistema de e-commerce, permitindo adicionar,
+  remover e visualizar itens no carrinho.
