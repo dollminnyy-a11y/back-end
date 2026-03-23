06d3685428060eb6dac10702c4c7e3a94233b799
dollminnyy-a11y <dollminnyy@gmail.com>
2026-03-23 11:37:51 -0300

    Initialize QR Code generator project documentation
    Add project description and GitHub link for QR Code generator.

---
+++ QR Codes. Node.js
@@ -0,0 +1,4 @@
+https://github.com/dollminnyy/qrcode-generator-node
+Este projeto consiste na criação de um gerador de QR Codes voltado para aplicações de e-commerce, desenvolvido utilizando Node.js. A ferramenta permite gerar códigos QR a partir de URLs,
+facilitando o acesso rápido a páginas de produtos,
+promoções ou pagamentos.
