2f7a94db8bd1cf434ba767a2b9d4d542e9272564
dollminnyy-a11y <dollminnyy@gmail.com>
2026-03-23 11:29:20 -0300

    Initialize Kart.Node.js with project details
    Add project description and repository link for Kart.Node.js

---
+++ o Kart.Node.js
@@ -0,0 +1,3 @@
+https://github.com/dollminnyy/mario-kart-simulator
+Este projeto consiste em um simulador de corridas inspirado no universo de Mario Kart, desenvolvido utilizando Node.js. A aplicação simula disputas entre personagens clássicos da franquia, aplicando regras de corrida,
+  aleatoriedade e lógica de pontuação.
