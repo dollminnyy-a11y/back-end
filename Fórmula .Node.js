5ce567706350a92b186a9cc9a8a8bac1cf5183aa
dollminnyy-a11y <dollminnyy@gmail.com>
2026-03-23 14:10:26 -0300

    Initialize Fórmula .Node.js project with description
    Add project description and GitHub link for Fórmula .Node.js

---
+++ Fórmula .Node.js
@@ -0,0 +1,3 @@
+https://github.com/dollminyy-a11y/f1-minimal-api
+Este projeto consiste na criação de uma Minimal API inspirada no universo da Formula 1, desenvolvida com Node.js e o framework Fastify. A aplicação fornece endpoints simples e
+  eficientes para consulta de dados relacionados a pilotos, equipes e corridas.
